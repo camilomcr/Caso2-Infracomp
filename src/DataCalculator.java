99347562a86eb114262a3c8f654eb39ab7bcfdff
camilo1008934 <c.morillo@uniandes.edu.co>
2024-04-09 21:57:17 -0500

    update data calc

--- src/DataCalculator.java
+++ src/DataCalculator.java
@@ -1,3 +1,7 @@
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.util.List;
 public class DataCalculator {
     private final int MP;
     private final String ReferenceFile;
@@ -13,8 +17,26 @@ public class DataCalculator {
     }
 
     public void CalculateData(){
+        int TP;
+        String filePath = "docs/"+ReferenceFile;
 
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(filePath));
+            for (String line : lines) {
+                String[] splitLine = line.split("=");
+                if (splitLine[0].equals("TP")){
+                    TP = Integer.parseInt(splitLine[1]);
+                }
 
-        
+                splitLine = line.split(",");
+                if (splitLine[3].equals("R") || splitLine[3].equals("W")){
+                    
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
